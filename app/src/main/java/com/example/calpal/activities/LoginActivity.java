509325370cdae77cc9a209c535b2b89503e5c443
ishaprabesh <iprabesh@purdue.edu>
2020-12-20 18:33:31 +0530

    Created GoogleSignInClient and google sign in button

--- app/src/main/java/com/example/calpal/activities/LoginActivity.java
+++ app/src/main/java/com/example/calpal/activities/LoginActivity.java
@@ -1,5 +1,47 @@
 package com.example.calpal.activities;
 
-public class LoginActivity {
+import android.os.Bundle;
+import android.view.View;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.calpal.R;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.SignInButton;
+
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
+    private GoogleSignInClient mGoogleSignInClient;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //Views
+
+        // Button listeners
+        findViewById(R.id.sign_in_button).setOnClickListener(this);
+
+        // Configure sign-in to request the user's ID, email address, and basic
+        // profile. ID and basic profile are included in DEFAULT_SIGN_IN.
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+
+        // Build a GoogleSignInClient with the options specified by gso.
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+
+        // Set the dimensions of the sign-in button.
+        SignInButton signInButton = findViewById(R.id.sign_in_button);
+        signInButton.setSize(SignInButton.SIZE_STANDARD);
+        signInButton.setColorScheme(SignInButton.COLOR_LIGHT);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+
+    }
 }
