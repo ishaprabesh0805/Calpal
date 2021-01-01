13486f5bae7da3b0864e7f0fede981d516993251
ishaprabesh <iprabesh@purdue.edu>
2021-01-01 23:44:54 +0530

    Completed fragment_add_event_page.xml

--- app/src/main/java/com/example/calpal/Fragments/AddEventPage.java
+++ app/src/main/java/com/example/calpal/Fragments/AddEventPage.java
@@ -41,26 +41,19 @@ public class AddEventPage extends Fragment {
     // TODO: Rename and change types and number of parameters
     public static AddEventPage newInstance(String param1, String param2) {
         AddEventPage fragment = new AddEventPage();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
         return fragment;
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+        // Links xml fragment_add_event_page.xml with this file
         return inflater.inflate(R.layout.fragment_add_event_page, container, false);
     }
 }
